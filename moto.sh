ca30fd687cd3623234f0444e5d9eb44ac864d4f7
yeonghan.woo <dudgks29@naver.com>
2018-03-01 12:19:55 +0900

    Revert "?"
    This reverts commit 9628d1eeffce6e8ba2e3469eba6634bbba328709.

--- moto.sh
+++
@@ -1,9 +0,0 @@
-LIST=$1
-
-. ./cpu_mem_check.sh ${LIST} >> testwoo1010.txt
-
-rcp -p testwoo1010.txt root@VCSYSTS1577:/root/environments/my_env/static/
-rsh -x -l root '. ./environments/my_env/bin/activate'
-rsh -x -l root 'python text-to-excel.py testwoo1010.txt'
-
-. ./environments/my_env/bin/activate | python environments/my_env/static/text-to-excel.py testwoo1010.txt
--- testmodule001.py
+++
@@ -1,6 +0,0 @@
-#TEST Module.py
-
-def sum(a,b):
-    return a+b
-
-def 
