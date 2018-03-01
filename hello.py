9628d1eeffce6e8ba2e3469eba6634bbba328709
yeonghan.woo <dudgks29@naver.com>
2018-03-01 12:19:18 +0900

    ?
    dad

---
+++ hello.py
@@ -0,0 +1,20 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+
+# def hello_world():
+#     return 'hello world!'
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
+    
+# @app.route('/test') 
+
+# def index():
+#     return 'index Page'
+
+import platform
+
+print(platform.platform())
+
